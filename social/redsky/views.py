032c1cb5d82905c5764551497be54b56e71fcf47
Adam Desmarais <98477564+adesmarais944@users.noreply.github.com>
2024-11-22 15:57:37 -0500

    added login/logout functionality

--- social/redsky/views.py
+++ social/redsky/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Profile, Chirp
 from .forms import ChirpForm
+from django.contrib.auth import authenticate, login, logout
 
 def home(request):
     if request.user.is_authenticated:
@@ -48,3 +49,23 @@ def profile(request, pk):
     else:
         messages.success(request, ('You must be logged in to view this page.'))
         return redirect('home')
+    
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('You have logged in.'))
+            return redirect('home')
+        else:
+            messages.success(request, ('There was an issue logging in. Please try again.'))
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('You have been logged out.'))
+    return redirect('home')
