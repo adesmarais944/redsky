8b90dd4451cdfbbd2f8c662f2a1341ed7a1a609e
Adam Desmarais <98477564+adesmarais944@users.noreply.github.com>
2024-11-21 22:54:32 -0500

    added profile_list view for authenticated users and users auto-following themselves

--- social/redsky/models.py
+++ social/redsky/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Profile(models.Model):
@@ -8,6 +9,18 @@ class Profile(models.Model):
         related_name="followed_by",
         symmetrical=False,
         blank=True)
+    date_modified = models.DateTimeField(User, auto_now=True)
     
     def __str__(self):
         return self.user.username
+    
+# Create Profile when new User signs up
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        # Have the User follow themselves
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+
+post_save.connect(create_profile, sender=User)
