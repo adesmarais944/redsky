e1a55b70c000aafd06f2b45874e37bce22a62a20
Adam Desmarais <98477564+adesmarais944@users.noreply.github.com>
2024-11-21 21:57:58 -0500

    updated admin util, cleaned navbar

--- social/redsky/admin.py
+++ social/redsky/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
 
-# Register your models here.
+# Unregister Groups
+admin.site.unregister(Group)
+
+# Add Profile info to User
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Only display username and profile fields
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+# Unregister initial User
+admin.site.unregister(User)
+
+# Reregister User and Profile
+admin.site.register(User, UserAdmin)
