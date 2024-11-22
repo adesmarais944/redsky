48d2a3d22ba2fe582c3a42020dcb96ba5df2418b
Adam Desmarais <98477564+adesmarais944@users.noreply.github.com>
2024-11-22 17:29:48 -0500

    added buggy update user feature

--- social/redsky/views.py
+++ social/redsky/views.py
@@ -4,6 +4,7 @@ from .models import Profile, Chirp
 from .forms import ChirpForm, SignUpForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django import forms
 
 def home(request):
@@ -89,3 +90,17 @@ def register_user(request):
             messages.success(request, ('You have successfully registered.'))
             return redirect('home')
     return render(request, 'register.html', {"form":form})
+
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        form = SignUpForm(request.POST or None, instance=current_user)
+        if form.is_valid():
+            form.save()
+            login(request, current_user)
+            messages.success(request, ('Your profile has been updated.'))
+            return redirect('home')
+        return render(request, 'update_user.html', {"form":form})
+    else:
+        messages.success(request, ('You must be logged in to view this page.'))
+        return redirect('home')
